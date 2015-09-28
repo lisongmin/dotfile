4d42d6f36f773f4ff9d366299d7748dc22d0f5cb
lisongmin <lisongmin@126.com>
2015-09-28 17:53:12 +0800

    filemanager set to nemo

--- qtile/config.py
+++ qtile/config.py
@@ -59,7 +59,7 @@ keys = [
 
     Key([mod], "Return", lazy.spawn("xfce4-terminal")),
     Key([mod, "shift"], "w", lazy.spawn("firefox")),
-    Key([mod, "shift"], "f", lazy.spawn("thunar")),
+    Key([mod, "shift"], "f", lazy.spawn("nemo --no-desktop")),
     Key([mod, "shift"], "m", lazy.spawn("thunderbird")),
     Key([mod, "shift"], "v", lazy.spawn("virt-viewer -c qemu:///system win10")),
 
