56bf726626663f6a80091688c89ae0fcff78f0cb
lisongmin <lisongmin9@gmail.com>
2018-11-07 22:36:05 +0800

    add reset keymap to locale.sh

--- locale.sh
+++ locale.sh
@@ -2,3 +2,7 @@
 
 sudo localectl --no-convert set-keymap dvorak-programmer
 sudo localectl --no-convert set-x11-keymap us pc105 dvp terminate:ctrl_alt_bksp
+
+# reset 
+# sudo localectl --no-convert set-keymap us
+# sudo localectl --no-convert set-x11-keymap us pc105
