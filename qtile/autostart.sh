4934491fdc7a82bab782af691073df0612489021
lisongmin <lisongmin@protonmail.com>
2020-04-16 20:59:50 +0800

    do not autostart tunderbird.

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -39,10 +39,10 @@ if [ $? -ne 0 ]; then
     tor-browser&
 fi
 
-pgrep -U "$USER" '^thunderbird$'
-if [ $? -ne 0 ];then
-    thunderbird&
-fi
+#pgrep -U "$USER" '^thunderbird$'
+#if [ $? -ne 0 ];then
+#    thunderbird&
+#fi
 
 # pgrep osdlyrics
 # if [ $? -ne 0 ]; then
