1270497bbcf50c3a8d52bd6d1a4c7031840249b6
lisongmin <lisongmin@protonmail.com>
2020-10-07 11:02:08 +0800

    colorful ls command via vivid

---
+++ install_basic_pkgs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# terminals
+sudo pacman -S --needed --noconfirm termite xfce4-terminal
+
+# shell
+sudo pacman -S --needed --noconfirm zsh tmux
+# colorful the `ls` command
+sudo pacman -S --needed --noconfirm vivid
+
+# text editor
+sudo pacman -S --needed --noconfirm neovim vim
