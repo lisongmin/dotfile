590e918b4f8652525e42e9424a9113ea4752776d
Songmin Li <lisongmin@protonmail.com>
2023-10-28 10:26:59 +0800

    Add copilot support in nvim

---
+++ _config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,104 @@
+-- Add a log function
+local function log_it(message)
+  vim.notify(message)
+end
+
+-- This function checks if the matching_file matches
+-- any of the patterns in the pattern_file.
+-- It will return true if matched else false.
+local function is_match_pattern(matching_file, pattern_file)
+  -- Check if pattern_file exists
+  if vim.fn.filereadable(pattern_file) == 1 then
+    local patterns = vim.fn.readfile(pattern_file)
+    local current_dir = vim.fn.fnamemodify(pattern_file, ":h")
+    for _, pattern in ipairs(patterns) do
+      -- trim the pattern first
+      pattern = vim.trim(pattern)
+      -- Ignore empty lines and lines that starts with '#'
+      if pattern ~= "" and not vim.startswith(pattern, "#") then
+        local full_pattern = current_dir .. (vim.startswith(pattern, "/") and pattern or "/" .. pattern)
+        if vim.fn.match(matching_file, full_pattern) ~= -1 then
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
+-- This function checks if the current file should be attached to copilot.
+-- It checks for the existence of .copilotallow and .copilotignore files
+-- in the current directory and all its parent directories.
+--
+-- * If .copilotallow file is found, it will check if the current file matches
+-- any of the patterns in the .copilotallow file and return true if it matches.
+-- * If .copilotignore file is found, it will check if the current file matches
+-- any of the patterns in the .copilotignore file and return false if it matches.
+-- * Repeat above two steps for all parent directories until the root directory is reached.
+-- * If none of the above files are found, it will return true.
+local function should_attach_copilot()
+  local current_file = vim.fn.expand("%:p")
+  if current_file == "" then
+    return false, "Disallow copilot for no current file"
+  end
+
+  local current_dir = vim.fn.fnamemodify(current_file, ":h")
+
+  repeat
+    local copilotallow = current_dir .. "/.copilotallow"
+    if is_match_pattern(current_file, copilotallow) then
+      return true, "Allow copilot by " .. copilotallow
+    end
+
+    local copilotignore = current_dir .. "/.copilotignore"
+    if is_match_pattern(current_file, copilotignore) then
+      return false, "Disallow copilot by " .. copilotignore
+    end
+    current_dir = vim.fn.fnamemodify(current_dir, ":h")
+  until current_dir == "" or current_dir == "/"
+
+  return true, "Allow copilot by default"
+end
+
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        ["*"] = function()
+          local enabled_filetypes = {
+            "markdown",
+            "help",
+            "python",
+            "go",
+            "rust",
+            "lua",
+            "yaml",
+            "ansible.yaml",
+            "html",
+            "css",
+            "typescript",
+            "javascript",
+          }
+
+          local buf = vim.api.nvim_get_current_buf()
+          if vim.b[buf].should_attach_copilot_flag == nil then
+            -- Only attach to copilot if the current file is not ignored
+            -- and filetype matched in list enabled_filetypes
+            local allow_attach, reason = should_attach_copilot()
+            if allow_attach then
+              allow_attach = vim.tbl_contains(enabled_filetypes, vim.bo.filetype)
+              if not allow_attach then
+                reason = reason .. "\nDisallow copilot by the filetype " .. vim.bo.filetype
+              end
+            end
+            log_it(reason)
+            vim.b[buf].should_attach_copilot_flag = allow_attach
+          end
+
+          return vim.b[buf].should_attach_copilot_flag
+        end,
+      },
+    },
+  },
+}
