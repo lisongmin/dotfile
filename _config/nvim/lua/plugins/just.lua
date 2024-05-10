16747893977f490c645640b37e2933572527d5fe
Songmin Li <lisongmin@protonmail.com>
2024-05-10 12:12:54 +0800

    feat(nvim-treesitter): add 'just' to ensured installed languages

--- _config/nvim/lua/plugins/just.lua
+++ _config/nvim/lua/plugins/just.lua
@@ -1,3 +1,11 @@
 return {
   "NoahTheDuke/vim-just",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "just",
+      })
+    end,
+  },
 }
