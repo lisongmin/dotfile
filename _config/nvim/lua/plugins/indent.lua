a6ec5f0b2835379a399047fa4cc7ba4f975dd13b
Songmin Li <lisongmin@protonmail.com>
2024-05-18 12:45:50 +0800

    feat(nvim): update plugins version and configurations

--- _config/nvim/lua/plugins/indent.lua
+++ _config/nvim/lua/plugins/indent.lua
@@ -1,4 +1,3 @@
 return {
-  "editorconfig/editorconfig-vim",
   "conormcd/matchindent.vim",
 }
--- _config/nvim/lua/plugins/yazi.lua
+++ _config/nvim/lua/plugins/yazi.lua
@@ -1,24 +1,30 @@
 ---@type LazySpec
 return {
-  "mikavilpas/yazi.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  event = "VeryLazy",
-  config = function(_, opts)
-    require("yazi").setup(opts)
-  end,
-  keys = {
-    {
-      "-",
-      function()
-        require("yazi").yazi()
-      end,
-      { desc = "Open the file manager" },
+  {
+    "mikavilpas/yazi.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("yazi").setup(opts)
+    end,
+    keys = {
+      {
+        "-",
+        function()
+          require("yazi").yazi()
+        end,
+        { desc = "Open the file manager" },
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      open_for_directories = true,
     },
   },
-  ---@type YaziConfig
-  opts = {
-    open_for_directories = true,
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
   },
 }
