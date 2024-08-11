dedc9e48787fa8314cd9e3d1cf446a0a83c607b7
Songmin Li <lisongmin@protonmail.com>
2024-08-11 11:10:41 +0800

    feat(just): link files in home via 'just link home-all'

--- link.sh
+++ link.sh
@@ -3,32 +3,13 @@
 _dir=$(realpath $(dirname $0))
 source ${_dir}/.link_lib
 
-# python linters
-link {_,~/.}pylintrc
-link {_,~/.}xprofile
-
-# vim relative
-link {_,~/.}ctags
-
 # tmux
 if [ ! -d ~/.tmux/plugins/tpm ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-link {_,~/.}tmux.conf
-
-# zsh
-link {_,~/.}zshrc
-link {_,~/.}p10k.zsh
-link {_,~/.}remote_p10k.zsh
 
 # pip
 link pip.conf ~/.pip/pip.conf
-
-# cargo
-link _cargo ~/.cargo/config
-
-# npm
-link _npmrc ~/.npmrc
 
 # build arch package in chroot
 link {_,~/.}local/bin/buildpkg
