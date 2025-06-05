113b013ca05949ed52c6f3bbade77023c251c10c
Songmin Li <lisongmin@protonmail.com>
2025-06-05 22:49:52 +0800

    fix android captive portal on china

--- utils/android_set_captive_portal.sh
+++ utils/android_set_captive_portal.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-adb shell "settings put global captive_portal_https_url https://connect.rom.miui.com/generate_204"
-adb shell "settings put global captive_portal_http_url http://connect.rom.miui.com/generate_204"
+adb shell settings put global captive_portal_use_https 1
+adb shell settings put global captive_portal_https_url https://connect.rom.miui.com/generate_204
+adb shell settings put global captive_portal_http_url http://connect.rom.miui.com/generate_204
+adb shell settings put global captive_portal_mode 0
