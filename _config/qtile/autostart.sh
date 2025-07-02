b3ecb06d3959fa0b75e3b455741d38d8b6291f97
Songmin Li <lisongmin@protonmail.com>
2025-07-02 12:57:41 +0800

    update qtile autostart

--- _config/qtile/autostart.sh
+++ _config/qtile/autostart.sh
@@ -5,9 +5,16 @@ echo "$(date -Is) Starting user autostart script ..."
 run-daemon() {
   program="$1"
   shift # Remove first argument (program name)
+
   exec_path=$(which "$program")
   if [ -e "$exec_path" ]; then
-    pgrep -U "$USER" "^$program$"
+    # Use -f option if program name is longer than 15 characters
+    if [ ${#program} -gt 15 ]; then
+      pgrep -U "$USER" -f "\<$program\>"
+    else
+      pgrep -U "$USER" "^$program$"
+    fi
+
     if [ $? -ne 0 ]; then
       echo "$(date -Is) Running daemon $program ..."
       "$exec_path" "$@" &
@@ -21,7 +28,7 @@ systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 xset -b
 
 # autorandr do not support wayland yet
-autorandr --change
+# autorandr --change
 
 systemctl --user start dunst
 
@@ -33,5 +40,9 @@ run-daemon Telegram
 # run-daemon thunderbird
 # run-daemon osdlyrics
 # run-daemon element-desktop
+
+if [ -z "$WAYLAND_DISPLAY" ]; then
+  run-daemon xfce4-screensaver
+fi
 
 echo "$(date -Is) User autostart script finished."
